799f83afef59abe05aa244f01933ef62702a61fd
Mash.dev <103385466+Alhassan33@users.noreply.github.com>
2026-04-11 17:40:48 +0100

    gate tools behind apply form

--- website/build.js
+++ website/build.js
@@ -1,18 +1,9 @@
 const fs = require("fs");
 
-const clientId   = process.env.CLIENT_ID;
 const twitterUrl = process.env.TWITTER_URL || "#";
 const discordUrl = process.env.DISCORD_URL || "#";
 
-if (!clientId) {
-  console.error("CLIENT_ID env var not set");
-  process.exit(1);
-}
-
-const inviteUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&scope=bot+applications.commands&permissions=8`;
-
 let html = fs.readFileSync("index.html", "utf8");
-html = html.replaceAll("{{INVITE_URL}}", inviteUrl);
 html = html.replaceAll("{{TWITTER_URL}}", twitterUrl);
 html = html.replaceAll("{{DISCORD_URL}}", discordUrl);
 
